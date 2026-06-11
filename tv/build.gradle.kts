fab63bfffee5b6e3a4d0da4b4614a3c9bf22156e
Vikas MacBook Air <vikastiwari199915@gmail.com>
2026-06-11 13:00:52 +0530

    Update default application version to 1.3.4

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     val releaseVersionName: String =
-        (project.findProperty("versionName") as String?) ?: "1.3.3"
+        (project.findProperty("versionName") as String?) ?: "1.3.4"
 
     defaultConfig {
         applicationId = "com.vikas.torrentplayer.tv"
